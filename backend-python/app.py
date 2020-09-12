c210ac9ff13ef795b452f6b94b82ca17bb9eb05b
Mateusz Zukowski <mszzukowski@gmail.com>
2020-09-12 15:21:00 +0000

    Working backend REST dummy

--- backend-python/app.py
+++ backend-python/app.py
@@ -1,14 +1,45 @@
 #!flask/bin/python
 #initial app from https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 # helping with https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
-# NOT WORKING AT ALL
-from flask import Flask
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+clients = [
+        {
+            'id': 0,
+            'name': 'Agata',
+            'lashes': '8-9-10-11B-11-12-11-10C'
+        },
+
+        {
+            'id': 1,
+            'name': 'Ania',
+            'lashes': '8-9-11-12-13-12'
+        },
+
+        {
+            'id': 2,
+            'name': 'Lena',
+            'lashes': '8-9-10-11-10-9'
+        },
+
+        {
+            'id': 3,
+            'name': 'Mariola',
+            'lashes': '8-9-10-11-12-13-12-11-10-9-8'
+        }
+]
+
 @app.route('/')
 def index():
-        return "Hello, World!"
+    return "Hello, World!"
 
-    if __name__ == '__main__':
-            app.run(debug=True)
+@app.route('/api/v1.0/clients', methods=['GET'])
+def get_clients():
+    return jsonify({'clients': clients})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- front/script.js
+++ front/script.js
@@ -26,3 +26,24 @@ function add_visit(){
 	document.getElementById("client").innerHTML = sc
 		+ ' data:' + date + ' notes:' + notes;
 }
+
+
+function get_clients(){
+	var request = new XMLHttpRequest()
+
+	request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+	request.onload = function () {
+		  // Begin accessing JSON data here
+   var data = JSON.parse(this.response)
+
+     if (request.status >= 200 && request.status < 400) {
+         data.forEach((movie) => {
+               console.log(movie.title)
+         })
+     } else {
+         console.log('error')
+     }
+}
+
+request.send()
+}
