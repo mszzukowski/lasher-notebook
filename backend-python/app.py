62a1c1467ac790633bad1e34969ccc83b7490d0f
Mateusz Zukowski <mszzukowski@gmail.com>
2020-09-13 15:13:38 +0000

    dummy backend - working version

--- backend-python/app.py
+++ backend-python/app.py
@@ -36,28 +36,28 @@ visits = [
         {
             'id': 0,
             'client_id': 0,
-            'parameters': '8-9-10-11B-11-12-11-10C',
+            'parameters': [8,9,10,11, 'C',11,12,11,10,'B'],
 			'date' : '10-10-2020'
         },
 
         {
             'id': 1,
             'client_id': 1,
-            'parameters': '8-9-11-12-13-12',
+            'parameters': [8,9,11,12,13,12],
 			'date': '09-09-2020'
         },
 
         {
             'id': 2,
             'client_id': 2,
-            'parameters': '8-9-10-11-10-9',
+            'parameters': [8,9,10,11,10,9],
 			'date': '11-08-2020'
         },
 
         {
             'id': 3,
             'client_id': 3,
-            'parameters': '8-9-10-11-12-13-12-11-10-9-8',
+            'parameters': [8,9,10,11,12,13,12,11,10,9,8],
 			'date': '12-09-2020'
         }
 ]
@@ -67,7 +67,7 @@ def get_clients():
     return jsonify({'clients': clients})
 
 @app.route('/api/v1.0/visits', methods=['GET'])
-def get_clients():
+def get_visitss():
     return jsonify({'visits': visits})
 
 if __name__ == '__main__':
