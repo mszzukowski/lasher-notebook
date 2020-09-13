f1db03a8efa8d1f976e75a5bd899fdbdc4041343
Mateusz Zukowski <mszzukowski@gmail.com>
2020-09-13 16:59:04 +0200

    changed dummy backend jsons

--- backend-python/app.py
+++ backend-python/app.py
@@ -10,32 +10,65 @@ clients = [
         {
             'id': 0,
             'name': 'Agata',
-            'lashes': '8-9-10-11B-11-12-11-10C'
+            'surname': 'Kot'
         },
 
         {
             'id': 1,
             'name': 'Ania',
-            'lashes': '8-9-11-12-13-12'
+            'surname': 'Pies'
         },
 
         {
             'id': 2,
             'name': 'Lena',
-            'lashes': '8-9-10-11-10-9'
+            'lashes': 'Kurka'
         },
 
         {
             'id': 3,
             'name': 'Mariola',
-            'lashes': '8-9-10-11-12-13-12-11-10-9-8'
+            'surname': 'Wąż'
         }
 ]
 
+visits = [
+        {
+            'id': 0,
+            'client_id': 0,
+            'parameters': '8-9-10-11B-11-12-11-10C',
+			'date' : '10-10-2020'
+        },
+
+        {
+            'id': 1,
+            'client_id': 1,
+            'parameters': '8-9-11-12-13-12',
+			'date': '09-09-2020'
+        },
+
+        {
+            'id': 2,
+            'client_id': 2,
+            'parameters': '8-9-10-11-10-9',
+			'date': '11-08-2020'
+        },
+
+        {
+            'id': 3,
+            'client_id': 3,
+            'parameters': '8-9-10-11-12-13-12-11-10-9-8',
+			'date': '12-09-2020'
+        }
+]
 
 @app.route('/api/v1.0/clients', methods=['GET'])
 def get_clients():
     return jsonify({'clients': clients})
+
+@app.route('/api/v1.0/visits', methods=['GET'])
+def get_clients():
+    return jsonify({'visits': visits})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
